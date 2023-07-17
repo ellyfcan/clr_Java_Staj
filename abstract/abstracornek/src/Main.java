76b59c9287306e82f079f75c9c266c88c0884aac
ellyfcan <eefil.eh@gmail.com>
2023-07-17 16:56:42 +0300

    abstract konusu

---
+++ abstract/abstracornek/src/Main.java
@@ -0,0 +1,8 @@
+
+public class Main {
+    public static void main(String[] args) {
+        WomanCalculator womanCalculator=new WomanCalculator();
+        womanCalculator.hesapla();
+        womanCalculator.gameOver();
+    }
+}
